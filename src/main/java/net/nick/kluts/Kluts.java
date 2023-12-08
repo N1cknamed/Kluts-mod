382666876dce8728ac05f722ea520dc84e680b05
Robbie Kooistra <robbiekooistra2000@gmail.com>
2023-12-08 15:10:09 +0100

    Initial commit

---
+++ src/main/java/net/nick/kluts/Kluts.java
@@ -0,0 +1,16 @@
+package net.nick.kluts;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Kluts implements ModInitializer {
+	public static final String MOD_ID = "kluts";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/nick/kluts/KlutsClient.java
@@ -0,0 +1,10 @@
+package net.nick.kluts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KlutsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
